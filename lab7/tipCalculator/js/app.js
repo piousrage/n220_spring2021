89af720c148ee826b6e99bc3555a8682572e63eb
piousrage <77476706+piousrage@users.noreply.github.com>
2021-04-01 21:01:08 -0400

    updated and fixed sum issues present within code

--- lab7/tipCalculator/js/app.js
+++ lab7/tipCalculator/js/app.js
@@ -1,21 +1,21 @@
 document.querySelector("#tip-form").onchange = function () {
   var bill = Number(document.getElementById("billTotal").value);
 
-  var tip = document.getElementById("billTotal").value;
-  document.getElementById("tipOutput").innerHTML = "${tip}%";
+  var tip = document.getElementById("tipInput").value;
+  document.getElementById("tipOutput").innerHTML = `${tip}%`;
 
   var tipValue = bill * (tip / 100);
 
-  var finalBill = bill * tipValue;
+  var finalBill = bill + tipValue;
 
   console.log(finalBill);
 
   var tipAmount = document.querySelector("#tipAmount");
 
-  var totalBillWithTip = document.querySelector("#totalBillWithTip");
+  var totalBill = document.querySelector("#total");
 
   tipAmount.value = tipValue.toFixed(2);
-  totalBillWithTip.value = finalBill.toFixed(2);
+  totalBill.value = finalBill.toFixed(2);
 
   document.getElementById("results").style.display = "block";
 };
